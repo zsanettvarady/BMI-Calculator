a30621fd1c824be8c8c88fc9d5212743920f6da0
zsanettvarady <47211665+zsanettvarady@users.noreply.github.com>
2019-04-27 15:41:47 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+function calculateBmi() {
+
+  var weight = document.querySelector('.weightInput').value;
+  var height = document.querySelector('.heightInput').value;
+
+  if(weight > 0 && height > 0){
+    var finalBmi = weight/(height/100*height/100);
+    document.querySelector('.bmi').textContent = finalBmi.toFixed(1);
+      if(finalBmi < 18.5){
+      document.querySelector('.text').textContent = "This means that you are too thin."
+      }
+      if(finalBmi > 18.5 && finalBmi < 25){
+      document.querySelector('.text').textContent = "This means that you are healthy."
+      }
+      if(finalBmi > 25){
+      document.querySelector('.text').textContent = "This means that you have overweight."
+      }
+  }
+  else{
+  alert("Please Fill in everything correctly")
+  }
+}
